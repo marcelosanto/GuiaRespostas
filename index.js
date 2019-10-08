66493e2e911b29f96d5567658fa6710ab9e5c3b6
Marcelo Santos <rocha.mer21@gmail.com>
2019-10-08 20:36:04 -0300

    if e forEach

--- index.js
+++ index.js
@@ -3,12 +3,26 @@ const app = express()
 
 app.set('view engine', 'ejs')
 
-app.get('/', (req, res) => {
-  const nome = 'Marcello'
-  const lang = 'Javascript'
+app.get('/:nome/:lang', (req, res) => {
+  const nome = req.params.nome
+  const lang = req.params.lang
+  const exibirMsg = false
+
+  const produtos = [
+    {nome: 'relogio', preco: 80},
+    {nome: 'doritos', preco: 10},
+    {nome: 'goiaba', preco: 1.50},
+    {nome: 'pulseira', preco: 25},
+    {nome: 'TV', preco: 1.800},
+    {nome: 'Celular', preco: 1.350},
+  ]
+
   res.render('index', {
     nome,
-    lang
+    lang,
+    empresa: 'Mrs Labs',
+    msg: exibirMsg,
+    produtos
   })
 })
 
