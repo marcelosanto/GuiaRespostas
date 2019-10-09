e32fdad817ede37be6171307f2c4763a93c97581
Marcelo Santos <rocha.mer21@gmail.com>
2019-10-09 11:42:56 -0300

    add bootstrap e limpeza do projeto

--- index.js
+++ index.js
@@ -2,28 +2,10 @@ const express = require('express')
 const app = express()
 
 app.set('view engine', 'ejs')
+app.use(express.static('public'))
 
-app.get('/:nome/:lang', (req, res) => {
-  const nome = req.params.nome
-  const lang = req.params.lang
-  const exibirMsg = false
-
-  const produtos = [
-    {nome: 'relogio', preco: 80},
-    {nome: 'doritos', preco: 10},
-    {nome: 'goiaba', preco: 1.50},
-    {nome: 'pulseira', preco: 25},
-    {nome: 'TV', preco: 1.800},
-    {nome: 'Celular', preco: 1.350},
-  ]
-
-  res.render('index', {
-    nome,
-    lang,
-    empresa: 'Mrs Labs',
-    msg: exibirMsg,
-    produtos
-  })
+app.get('/', (req, res) => {
+  res.render('index')
 })
 
 app.listen(8080, () => {
