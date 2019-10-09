ff1a28ec57c75203f0fd2146010653f5aff1e9cc
Marcelo Santos <rocha.mer21@gmail.com>
2019-10-09 13:10:42 -0300

    feita pagina de perguntar com bootstrap

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get('/', (req, res) => {
   res.render('index')
 })
 
+app.get('/perguntar', (req, res) => {
+  res.render('perguntar')
+})
+
 app.listen(8080, () => {
   console.log('servidor rodando')
 })
