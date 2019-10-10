e4f73d5e0d94bab28f2495f8755b6b816eef968d
Marcelo Santos <rocha.mer21@gmail.com>
2019-10-10 15:16:57 -0300

    Ordenação com o Sequelize

---
+++ database/Perguntas.js
@@ -0,0 +1,17 @@
+const Sequelize = require('sequelize')
+const connection = require('./database')
+
+const Perguntas = connection.define('perguntas',{
+  titulo:{
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  descricao:{
+    type: Sequelize.TEXT,
+    allowNull: false
+  }
+})
+
+Perguntas.sync({force: false}).then(() => {})
+
+module.exports = Perguntas
--- index.js
+++ index.js
@@ -1,15 +1,52 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 
+const connection = require('./database/database')
+const Perguntas = require('./database/Perguntas')
+
+//Database
+connection
+  .authenticate()
+  .then(() => {
+    console.log('Conexao feita com o banco de dados!')
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+
+// Isto indica para o Express usar o 'ejs' como engine html
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
+//Body Parser
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// Rotas
 app.get('/', (req, res) => {
-  res.render('index')
+  Perguntas.findAll({ raw: true, order:[
+    ['id', 'DESC']
+  ] }).then(perguntas => {
+    res.render('index',{
+      perguntas
+    })
+  })
 })
 
 app.get('/perguntar', (req, res) => {
   res.render('perguntar')
+})
+
+app.post('/salvarperguntar', (req, res) => {
+  const titulo = req.body.titulo
+  const descricao = req.body.descricao
+  Perguntas.create({
+    titulo,
+    descricao
+  }).then(() => {
+    res.redirect('/')
+  })
 })
 
 app.listen(8080, () => {
